1ecfd560309dbeb256af96a5669c9bacd6346c61
Faizan Haider <haiderfaizan732@gmail.com>
2021-10-26 03:07:25 +0530

    feat: Sets up initial project & gradle dependencies

---
+++ app/src/main/java/com/example/stackoverflobiz/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.stackoverflobiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stackoverflobiz.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
